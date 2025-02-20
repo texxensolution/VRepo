d8ffa6771ad3acff75c25533475a358b948e0618
jerome <jdluciano@spmadridlaw.com>
2025-02-20 17:02:32 +0800

    feat: update code

--- app/src/main/java/com/spmadrid/vrepo/presentation/screens/LoginScreen.kt
+++ app/src/main/java/com/spmadrid/vrepo/presentation/screens/LoginScreen.kt
@@ -2,17 +2,29 @@ package com.spmadrid.vrepo.presentation.screens
 
 import android.app.Activity
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.spmadrid.vrepo.domain.services.AuthenticationService
-import com.spmadrid.vrepo.presentation.viewmodel.AuthenticateViewModel
+
 
 @Composable
 fun LoginScreen(
@@ -22,21 +34,37 @@ fun LoginScreen(
     val activity = context as? Activity
 
     LaunchedEffect(Unit) {
-        activity?.let { authenticationService.initialize(activity) }
+        activity?.let {
+            authenticationService.initialize(activity)
+        }
     }
 
     Box(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Button(
-            modifier = Modifier
-                .padding(8.dp),
-            onClick = {
-                authenticationService.openLarkSSO()
-            }
+        Column(
+            modifier = Modifier.padding(horizontal = 5.dp)
         ) {
-            Text("Login with Lark")
+            Text(
+                "Welcome to VisionAI",
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp,
+                textAlign = TextAlign.Center
+            )
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue,
+                    contentColor = Color.White
+                ),
+                onClick = { authenticationService.openLarkSSO() },
+            ){
+                Text("Login with Lark")
+            }
         }
     }
 }
